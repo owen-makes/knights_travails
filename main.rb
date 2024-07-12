a7347f70e22db5b5aa5af57cbb022332ee7cce19
owen-makes <owen.botto@gmail.com>
2024-07-12 17:04:53 -0300

    Do everything over, make it work using BFS and simplifying the code for the specific purpose

--- main.rb
+++ main.rb
@@ -1,30 +1,50 @@
-
 class Knight
-  attr_accessor :x, :y, :board, :moves
+  MOVE_OFFSETS = [
+    [-2, -1], [-2, 1], [2, -1], [2, 1],
+    [-1, -2], [1, -2], [-1, 2], [1, 2]
+  ]
 
-  def initialize(x = 3, y = 3)
-    @x = x
-    @y = y
-    @board = Array.new(8) {Array.new(8)}
-    @moves = possible_moves
+  def initialize(board_size = 8)
+    @board_size = board_size
   end
 
-  def possible_moves(initial = [@x,@y])
-    #populate possible moves
-    pm = [    
-      [@x - 2, @y - 1],
-      [@x - 2, @y + 1],
-      [@x + 2, @y - 1],
-      [@x + 2, @y + 1],
-      [@x + 1, @y - 2],
-      [@x - 1, @y - 2],
-      [@x + 1, @y + 2],
-      [@x - 1, @y + 2]]
-    #reject ilegal moves
-    pm.reject {|x,y| x < 0 || y < 0 || x > 7 || y > 7}
+  def minimum_moves(start, target)
+    queue = [[start, 0, [start]]]  # [position, moves, path]
+    visited = Set.new([start])
+
+    until queue.empty?
+      position, moves, path = queue.shift
+
+      if position == target
+        return "You made it in #{moves} moves!\nHere's your path #{path}"
+      end
+
+      possible_moves(position).each do |next_position|
+        next if visited.include?(next_position)
+
+        new_path = [path] << next_position
+        queue << [next_position, moves + 1, new_path]
+        visited.add(next_position)
+      end
+    end
+
+    -1 # If target is unreachable
+  end
+
+  private
+
+  def possible_moves(coord)
+    MOVE_OFFSETS.map { |dx, dy| [coord[0] + dx, coord[1] + dy] }
+                .select { |x, y| valid_position?(x, y) }
+  end
+
+  def valid_position?(x, y)
+    x.between?(0, @board_size - 1) && y.between?(0, @board_size - 1)
   end
 end
 
-test = Knight.new(0,7)
-
-p test.moves.length
+# Example usage
+knight = Knight.new
+start = [3, 3]
+target = [6, 6]
+puts knight.minimum_moves(start, target)
